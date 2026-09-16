7beba782f8e21a8be6628f9892175e054cfe4827
NoirHare <177549718+NoirHare@users.noreply.github.com>
2026-09-16 20:45:32 +0800

    0

--- dsh.bat
+++ dsh.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 set "PATH=%~dp0runtime\node_modules\.bin;%~dp0runtime;%PATH%"
-set "DSH_HOME=%~dp0..\home"
+set "DSH_HOME=%~dp0home"
 call "%~dp0runtime\node_modules\.bin\dsh.cmd" %*
